2c9042803281e8b32c18d3e2bc99b27fde7bf907
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2022-01-05 10:28:26 -0300

    prepara revisao

--- src/styles/utils/noHighlight.js
+++
@@ -1,8 +0,0 @@
-import { css } from 'styled-components/macro';
-
-const unselectable = () => css`
-  -webkit-tap-highlight-color: transparent;
-  -webkit-touch-callout: none;
-`;
-
-export default unselectable;
